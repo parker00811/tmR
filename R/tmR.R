87aeea0b81d042380c6cb995e1c837a92bacd2a6
andrew.tang <sulaxd@gmail.com>
2015-03-29 16:51:05 +0800

    JSONdoc

---
+++ R/tmR.R
@@ -0,0 +1,7 @@
+JSONdoc <- toJSON(StoreJSON, digits = 10)
+writeUtf8 <- function(x, file, bom=F) {
+  con <- file(file, "wb")
+  if(bom) writeBin(BOM, con, endian="little")
+  writeBin(charToRaw(x), con, endian="little")
+  close(con)
+}
